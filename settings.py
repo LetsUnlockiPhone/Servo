a2c673fcd4e39210a81abd6b6a51660aba870b26
Filipp Lepalaan <filipp@mac.com>
2015-09-15 10:37:37 +0300

    Cleanup

--- settings.py
+++ settings.py
@@ -1,32 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013, First Party Software
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without modification, 
-# are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
-# and/or other materials provided with the distribution.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
-# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
-# SUCH DAMAGE.
 
 import os
 import re
 from django.contrib.messages import constants as messages
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
@@ -34,9 +13,6 @@ MESSAGE_TAGS = {
 
 BASE_DIR = os.path.dirname(__file__)
 APP_DIR = os.path.join(BASE_DIR, 'servo')
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('ServoApp Support', 'support@servoapp.com'),
@@ -240,7 +216,10 @@ REST_FRAMEWORK = {
 ENABLE_RULES = False
 TIMEZONE = 'Europe/Helsinki'
 
-from local_settings import *
+GSX_CERT = 'uploads/settings/gsx_cert.pem'
+GSX_KEY = 'uploads/settings/gsx_key.pem'
 
 os.environ['GSX_CERT'] = GSX_CERT
 os.environ['GSX_KEY'] = GSX_KEY
+
+from local_settings import *
