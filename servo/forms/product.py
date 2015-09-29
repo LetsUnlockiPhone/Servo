70fd37bd54ce30e6976aa7e374eed3b8e2c67c18
Filipp Lepalaan <filipp@mac.com>
2015-09-30 02:04:52 +0300

    Hide hidden users

--- servo/forms/product.py
+++ servo/forms/product.py
@@ -1,28 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013, First Party Software
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without modification, 
-# are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
-# and/or other materials provided with the distribution.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
-# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
-# SUCH DAMAGE.
 
 import re
 from django import forms
@@ -154,16 +130,16 @@ class PurchaseOrderSearchForm(forms.Form):
         required=False,
         label=_('State is'),
         choices=(
-            ('', _('Any')),
-            ('open', _('Open')),
-            ('submitted', _('Submitted')),
-            ('received', _('Received')),
+            ('',            _('Any')),
+            ('open',        _('Open')),
+            ('submitted',   _('Submitted')),
+            ('received',    _('Received')),
         ),
         widget=forms.Select(attrs={'class': 'input-small'})
     )
     created_by = forms.ModelChoiceField(
         required=False,
-        queryset=User.objects.filter(is_active=True)
+        queryset=User.active.all()
     )
     start_date = forms.DateField(
         required=False,
