2b622e5b17e86f1b3a050fbaee7f16a508566386
Filipp Lepalaan <filipp@mac.com>
2016-02-03 13:16:48 +0200

    Add custom exception for moving inventory to same location

--- servo/exceptions.py
+++ servo/exceptions.py
@@ -4,3 +4,8 @@
 class ConfigurationError(Exception):
     def __init__(self, arg):
         super(ConfigurationError, self).__init__(arg)
+
+
+class InventoryAlreadyAtLocation(Exception):
+    def __init__(self, arg):
+        super(InventoryAlreadyAtLocation, self).__init__(arg)
