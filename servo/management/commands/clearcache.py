d2677674eec9cebaa0748d8035cc1ac8329444e2
Filipp Lepalaan <filipp@mac.com>
2016-03-18 12:49:48 +0200

    Clear ALL caches, not just default

--- servo/management/commands/clearcache.py
+++ servo/management/commands/clearcache.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.core.cache import cache
+from django.core.cache import caches
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
-    help = "Clears this install's cache"
+    help = "Clears this install's caches"
 
     def handle(self, *args, **options):
-        cache.clear()
+        for c in caches.all():
+            c.clear()
+
+        exit(0)
